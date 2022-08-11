6a2c47a4e6d6b1788fb8b580dc9913a34fd0f3b3
Anthony <anthonycolas.france@gmail.com>
2022-08-11 14:55:42 +0200

    Feature: Connect to smart contract with Ethers.js

---
+++ utils/connectContract.js
@@ -0,0 +1,26 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+  const contactAddress = "0x0abDA4a161A355093DBe177da08BD7Bc7AA08BB8";
+  const contractABI = abiJSON.abi;
+  let rsvpContract;
+  try {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      // Checking for ETH object in the window
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      // Instantiating new connection to the contract
+      rsvpContract = new ethers.Contract(contractAddress, contractABI, signer);
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
+  } catch (error) {
+    console.log("ERROR:", error);
+  }
+  return rsvpContract;
+}
+
+export default connectContract;
